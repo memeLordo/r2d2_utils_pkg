83fe219d14ebd86d73f0eadbe439e66a9bcde076
memeLordo <serafim.kripachev@gmail.com>
2025-09-27 09:16:21 +0300

    Strings: Add transform_string()

--- include/r2d2_utils_pkg/Strings.hpp
+++ include/r2d2_utils_pkg/Strings.hpp
@@ -7,6 +7,22 @@
 #include <string_view>
 
 namespace r2d2_string {
+template <typename TransformFunc>
+[[nodiscard]]
+inline std::string transform_string(std::string_view sv, TransformFunc func,
+                                    size_t from = 0,
+                                    size_t to = std::string_view::npos) {
+  if (to == std::string_view::npos) to = sv.length();
+
+  assert(from <= sv.length());
+  assert(to >= from && to <= sv.length());
+
+  std::string str(sv);
+  auto begin_ = str.begin() + from;
+  auto end_ = str.begin() + to;
+  std::transform(begin_, end_, begin_, func);
+  return str;
+};
 [[nodiscard]]
 inline std::string lower(std::string_view sv) {
   std::string str(sv);
