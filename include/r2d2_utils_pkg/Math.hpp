1f222c292c6d712d7f558ad860ea402f057d680c
memeLordo <serafim.kripachev@gmail.com>
2025-09-28 03:06:05 +0300

    Math: Add type_traits

--- include/r2d2_utils_pkg/Math.hpp
+++ include/r2d2_utils_pkg/Math.hpp
@@ -5,34 +5,39 @@
 #include <cassert>
 #include <cmath>
 #include <cstdint>
+#include <type_traits>
 
 namespace r2d2_math {
 template <typename T>
-constexpr T min(const T a, const T b) {
+[[nodiscard]] constexpr T min(const T a, const T b) {
   return std::min(a, b);
 };
 template <typename T>
-constexpr T max(const T a, const T b) {
+[[nodiscard]] constexpr T max(const T a, const T b) {
   return std::max(a, b);
 };
 template <typename T>
-constexpr T abs(const T a) {
+[[nodiscard]] constexpr T abs(const T a) {
+  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
   return std::abs(a);
 };
 template <typename T>
-constexpr T deg2rad(const T a) {
-  return a * M_PI / 180.0;
+[[nodiscard]] constexpr T deg2rad(const T a) {
+  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
+  return a * T{M_PI} / T{180};
 };
 template <typename T>
-constexpr T sin(const T theta) {
+[[nodiscard]] constexpr T sin(const T theta) {
+  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
   return std::sin(deg2rad(theta));
 };
 template <typename T>
-constexpr T sqr(const T a) {
+[[nodiscard]] constexpr T sqr(const T a) {
+  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
   return a * a;
 };
 template <typename T>
-constexpr int8_t sign(const T a) {
+[[nodiscard]] constexpr int8_t sign(const T a) {
   static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
   return (a > T{0}) - (a < T{0});
 };
@@ -43,26 +48,26 @@ template <class Derived>
 class Wrapper {
  protected:
   template <typename T = double>
-  static constexpr T getRatio() {
+  [[nodiscard]] static constexpr T getRatio() {
     assert(Derived::s_convertRatio != 0);
     return static_cast<T>(Derived::s_convertRatio);
   };
 
  public:
   template <typename T>
-  static constexpr T wrap(const T value) {
+  [[nodiscard]] static constexpr T wrap(const T value) {
     return value / getRatio<T>();
   };
   template <typename T>
-  static constexpr T unwrap(const T rawValue) {
+  [[nodiscard]] static constexpr T unwrap(const T rawValue) {
     return rawValue * getRatio<T>();
   };
   template <typename T, typename T2>
-  static constexpr T wrap(const T2 value) {
+  [[nodiscard]] static constexpr T wrap(const T2 value) {
     return static_cast<T>(value / getRatio());
   };
   template <typename T, typename T2>
-  static constexpr T unwrap(const T2 rawValue) {
+  [[nodiscard]] static constexpr T unwrap(const T2 rawValue) {
     return static_cast<T>(rawValue * getRatio());
   };
 };
