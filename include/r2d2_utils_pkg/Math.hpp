366773f4ed5343abf9d52b90f92086573112a54a
memeLordo <serafim.kripachev@gmail.com>
2025-09-30 00:49:16 +0300

    Update static_assert checks

--- include/r2d2_utils_pkg/Math.hpp
+++ include/r2d2_utils_pkg/Math.hpp
@@ -8,6 +8,8 @@
 #include <type_traits>
 
 namespace r2d2_math {
+using std::is_arithmetic_v;
+
 template <typename T>
 [[nodiscard]] constexpr T min(const T a, const T b) {
   return std::min(a, b);
@@ -18,27 +20,27 @@ template <typename T>
 };
 template <typename T>
 [[nodiscard]] constexpr T abs(const T a) {
-  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
+  static_assert(is_arithmetic_v<T>, "abs: T must be an arithmetic type!");
   return std::abs(a);
 };
 template <typename T>
 [[nodiscard]] constexpr T deg2rad(const T a) {
-  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
+  static_assert(is_arithmetic_v<T>, "deg2rad: T must be an arithmetic type!");
   return a * T{M_PI} / T{180};
 };
 template <typename T>
 [[nodiscard]] constexpr T sin(const T theta) {
-  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
+  static_assert(is_arithmetic_v<T>, "sin: T must be an arithmetic type!");
   return std::sin(deg2rad(theta));
 };
 template <typename T>
 [[nodiscard]] constexpr T sqr(const T a) {
-  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
+  static_assert(is_arithmetic_v<T>, "sqr: T must be an arithmetic type!");
   return a * a;
 };
 template <typename T>
 [[nodiscard]] constexpr int8_t sign(const T a) {
-  static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
+  static_assert(is_arithmetic_v<T>, "sign: T must be an arithmetic type!");
   return (a > T{0}) - (a < T{0});
 };
 }  // namespace r2d2_math
