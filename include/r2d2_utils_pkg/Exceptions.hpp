14e2ffa05a78a855532812ca55586e50e9564760
memeLordo <serafim.kripachev@gmail.com>
2025-10-01 20:54:51 +0300

    Exceptions: Add SafeExceptionConfig class

--- include/r2d2_utils_pkg/Exceptions.hpp
+++ include/r2d2_utils_pkg/Exceptions.hpp
@@ -1,7 +1,13 @@
 #ifndef R2D2_EXCEPTIONS_HPP
 #define R2D2_EXCEPTIONS_HPP
 
+#include <stdexcept>
+
 namespace r2d2_exceptions {
+
+class SafeExceptionConfig {
+  static const std::exception_ptr s_exceptionPtr;
+};
 
 class Exception : public std::runtime_error {
  public:
