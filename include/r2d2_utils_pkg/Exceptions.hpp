d3c2ed2fa1b06f6aa31909fe1b5c6ebc297c44f0
memeLordo <serafim.kripachev@gmail.com>
2025-10-01 04:55:25 +0300

    Exceptions: exception handle -> err_msg handle

--- include/r2d2_utils_pkg/Exceptions.hpp
+++ include/r2d2_utils_pkg/Exceptions.hpp
@@ -2,7 +2,10 @@
 #define R2D2_EXCEPTIONS_HPP
 
 #include <exception>
+#include <iostream>
 #include <stack>
+
+#include "r2d2_utils_pkg/Debug.hpp"
 
 namespace r2d2_errors {
 class ExceptionStack : public std::runtime_error {
@@ -13,30 +16,25 @@ class ExceptionStack : public std::runtime_error {
 
 class ExceptionHandler {
  private:
-  static inline std::stack<std::exception_ptr> s_exceptionStack{};
+  static inline std::stack<std::string> s_exceptionStack{};
 
  public:
   static void check() {
     if (has_exceptions()) throw ExceptionStack{};
   };
-  template <typename Exception>
-  static void record(Exception&& e) {
-    try {
-      throw std::forward<Exception>(e);
-    } catch (...) {
-      s_exceptionStack.emplace(std::current_exception());
-    }
+  static void record(const std::exception& e) {
+    s_exceptionStack.emplace(e.what());
   };
   static void process_stack() {
-    while (!s_exceptionStack.empty()) try {
-        std::rethrow_exception(s_exceptionStack.top());
-      } catch (const std::exception& e) {
-        print_exception(e);
-        s_exceptionStack.pop();
-      }
+    while (has_exceptions()) {
+      print_exception(s_exceptionStack.top());
+      s_exceptionStack.pop();
+    }
+  };
+  static void print_exception(std::string_view err_msg) {
+    std::cerr << RED("Got exception: " << err_msg) << "\n";
   };
   static bool has_exceptions() { return !s_exceptionStack.empty(); };
-  static void print_exception(const std::exception& e);
 };
 }  // namespace r2d2_errors
 
