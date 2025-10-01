17a906832521a2530487892b979e1100483671fa
memeLordo <serafim.kripachev@gmail.com>
2025-10-01 20:52:48 +0300

    Exceptions: Add custom Exception class

--- include/r2d2_utils_pkg/Exceptions.hpp
+++ include/r2d2_utils_pkg/Exceptions.hpp
@@ -1,4 +1,12 @@
 #ifndef R2D2_EXCEPTIONS_HPP
 #define R2D2_EXCEPTIONS_HPP
 
+namespace r2d2_exceptions {
+
+class Exception : public std::runtime_error {
+ public:
+  explicit Exception(std::string message) : std::runtime_error(message) {}
+};
+}  // namespace r2d2_exceptions
+
 #endif  // R2D2_EXCEPTIONS_HPP
