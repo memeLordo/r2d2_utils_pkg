a1e6e47339423f08d33ac931eddbe99f709bea27
memeLordo <serafim.kripachev@gmail.com>
2025-11-03 01:20:54 +0300

    Add Console.hpp

--- include/r2d2_utils_pkg/Logging/DebugC.hpp
+++ include/r2d2_utils_pkg/Logging/DebugC.hpp
@@ -6,14 +6,7 @@
 #include <cstdint>
 #include <type_traits>
 
-#define ANSI_COLOR_RED "\033[0;31m"
-#define ANSI_COLOR_GREEN "\033[0;32m"
-#define ANSI_COLOR_YELLOW "\033[0;33m"
-#define ANSI_COLOR_BLUE "\033[0;34m"
-#define ANSI_COLOR_MAGENTA "\033[0;35m"
-#define ANSI_COLOR_CYAN "\033[0;36m"
-#define ANSI_COLOR_WHITE "\033[0;37m"
-#define ANSI_COLOR_RESET "\033[0m"
+#include "Console.hpp"
 
 constexpr std::size_t MAX_NAME_LEN{64};
 constexpr std::size_t MAX_VALUE_LEN{256};
