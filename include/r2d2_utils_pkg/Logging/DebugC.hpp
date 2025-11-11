ed5e0e065dd3dab74943e2551f88d5e93f09156c
memeLordo <serafim.kripachev@gmail.com>
2025-11-11 20:25:28 +0300

    Update DebugC.hpp

--- include/r2d2_utils_pkg/Logging/DebugC.hpp
+++ include/r2d2_utils_pkg/Logging/DebugC.hpp
@@ -22,12 +22,11 @@ constexpr void push_chr(char chr, char* dst, std::size_t& pos) {
 constexpr void end_str(char* dst, std::size_t& pos) { dst[pos] = '\0'; }
 
 [[nodiscard]]
-constexpr std::size_t num_to_str(std::int64_t value, char* buffer,
-                                 std::size_t max_len) {
+constexpr std::size_t num_to_str(std::int64_t value, char* buffer) {
   const bool negative{value < 0};
   if (negative) value = -value;
 
-  std::size_t idx{max_len - 1};
+  std::size_t idx{MAX_VALUE_LEN - 1};
   do {
     push_chr('0' + (value % 10), buffer, idx);
     value /= 10;
@@ -37,7 +36,7 @@ constexpr std::size_t num_to_str(std::int64_t value, char* buffer,
 
   // Copy to the beginning of the buffer
   std::size_t pos{0};
-  for (std::size_t i = idx + 1; i < max_len; ++i)
+  for (std::size_t i = idx + 1; i < MAX_VALUE_LEN; ++i)
     append_chr(buffer[i], buffer, pos);
   end_str(buffer, pos);
   return pos;
@@ -63,7 +62,8 @@ constexpr void concat_buff(const std::array<char, SrcSize>& src, char* dst,
 }
 
 template <std::size_t MaxNames>
-[[nodiscard]] constexpr std::array<char[MAX_NAME_LEN], MaxNames> parse_var_str(
+[[nodiscard]]
+constexpr std::array<char[MAX_NAME_LEN], MaxNames> parse_var_str(
     const char* var_str) {
   std::array<char[MAX_NAME_LEN], MaxNames> result{};
   std::size_t count{0}, start{0}, str_len{0};
@@ -91,7 +91,8 @@ template <std::size_t MaxNames>
 
 template <typename T>
 [[nodiscard]] constexpr std::array<char, MAX_VALUE_LEN> stream_var(
-    const char* var_name, const T& var_arg, std::size_t idx, std::size_t sz) {
+    const char* color, const char* var_name, const T& var_arg, std::size_t idx,
+    std::size_t sz) {
   std::array<char, MAX_VALUE_LEN> result{};
   std::size_t pos{0};
 
@@ -100,24 +101,19 @@ template <typename T>
     return result;
   }
 
-  copy_str(ANSI_COLOR_YELLOW, result.data(), pos, MAX_VALUE_LEN);
-  copy_str(var_name, result.data(), pos, MAX_VALUE_LEN);
-  copy_str(" = ", result.data(), pos, MAX_VALUE_LEN);
+  copy_str(color, result.data(), pos);
+  copy_str(var_name, result.data(), pos);
+  copy_str(" = ", result.data(), pos);
 
-  if constexpr (std::is_arithmetic_v<T>) {
-    char value_buf[MAX_VALUE_LEN]{};
-    const std::size_t value_len{num_to_str(static_cast<std::int64_t>(var_arg),
-                                           value_buf, MAX_VALUE_LEN)};
-
-    for (std::size_t i = 0; i < value_len && pos < MAX_VALUE_LEN - 1; ++i)
-      append_chr(value_buf[i], result.data(), pos);
+  if constexpr (std::is_arithmetic_v<T> || std::is_enum_v<T>) {
+    copy_arg(var_arg, result.data(), pos);
   } else if constexpr (std::is_same_v<std::decay_t<T>, const char*>) {
-    copy_str(var_arg, result.data(), pos, MAX_VALUE_LEN);
+    copy_str(var_arg, result.data(), pos);
   } else {
-    copy_str("<unknown>", result.data(), pos, MAX_VALUE_LEN);
+    copy_str("<unknown>", result.data(), pos);
   }
 
-  copy_str(ANSI_COLOR_RESET, result.data(), pos, MAX_VALUE_LEN);
+  copy_str(ANSI_GREEN, result.data(), pos);
   if (idx + 1 < sz) {
     append_chr(',', result.data(), pos);
     append_chr(' ', result.data(), pos);
@@ -128,7 +124,7 @@ template <typename T>
 
 template <typename... Args>
 [[nodiscard]] constexpr std::array<char, MAX_RESULT_LEN> stream_vars(
-    const char* var_str, const Args&... var_args) {
+    const char* color, const char* var_str, const Args&... var_args) {
   constexpr std::size_t sz{sizeof...(Args)};
   auto var_names{parse_var_str<sz>(var_str)};
 
@@ -136,8 +132,8 @@ template <typename... Args>
   std::size_t pos{0}, idx{0};
 
   auto append_var = [&](const auto& arg) {
-    concat_buff(stream_var(var_names[idx], arg, idx, sz), result.data(), pos,
-                MAX_RESULT_LEN);
+    concat_buff(stream_var(color, var_names[idx], arg, idx, sz), result.data(),
+                pos);
     ++idx;
   };
   (append_var(var_args), ...);
@@ -146,21 +142,13 @@ template <typename... Args>
 }
 
 template <typename... Args>
-constexpr auto stream_args_c(const char* label, const char* names,
+constexpr auto stream_args_c(const char* color, const char* names,
                              const Args&... args) {
   std::array<char, MAX_RESULT_LEN> result{};
   std::size_t pos{0};
 
-  if (label && label[0]) {
-    append_chr('[', result.data(), pos);
-    copy_str(ANSI_COLOR_MAGENTA, result.data(), pos, MAX_RESULT_LEN);
-    copy_str(label, result.data(), pos, MAX_RESULT_LEN);
-    copy_str(ANSI_COLOR_RESET, result.data(), pos, MAX_RESULT_LEN);
-    append_chr(']', result.data(), pos);
-    copy_str(" : ", result.data(), pos, MAX_RESULT_LEN);
-  }
-  auto vars{stream_vars(names, args...)};
-  concat_buff(vars, result.data(), pos, MAX_RESULT_LEN);
+  auto vars{stream_vars(color, names, args...)};
+  concat_buff(vars, result.data(), pos);
   end_str(result.data(), pos);
   return result;
 }
